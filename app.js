49c3c9ccb87ff0eeba9a002078a44dcc26e8d741
Mattias Welamsson <mattias@iMac.local>
2022-09-22 18:18:16 +0900

    playRound function added and code refractored

--- app.js
+++ app.js
@@ -1,23 +1,37 @@
+// Global Variables
+const playerSelections = playerSelection();
+const computerSelection = getComputerChoice();
+
+// Random Hand Function
+
 function getComputerChoice() {
     let choices = ["Rock", "Paper", "Scissor"];
     let randomChoice = Math.floor(Math.random() * choices.length);
     return choices[randomChoice];
-
 }
+
+// Player Input and formatting
 
 function playerSelection() {
  let input = prompt("Rock, Paper or Scissors?");
  input = input.toLowerCase();
- formattedChoice = input.charAt(0).toUpperCase() + input.slice(1);
-
- if (formattedChoice == getComputerChoice()) {
-    console.log(`Damn son. You got that bitch with your badass ${formattedChoice}!!`);
- }
- else {
-    console.log(`Computer picked: ${getComputerChoice()}...You lose`);
- }
-
- return formattedChoice 
+ return input.charAt(0).toUpperCase() + input.slice(1);
 }
 
-playerSelection();
+
+//Calls a new round + prints out win/loose message.
+
+function playRound(playerSelection, computerSelection) {
+   
+   if (playerSelection == computerSelection) {
+   console.log(`Damn son. You got that bitch with your badass ${playerSelection}!!`);
+}
+else {
+   console.log(`Computer picked: ${computerSelection}...You lose`);
+}
+}
+
+
+// Play
+
+playRound(playerSelections, computerSelection)
