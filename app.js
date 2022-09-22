d08c04919718b1028117a2d8e0cc7f0a18b89fa3
Mattias Welamsson <mattias@iMac.local>
2022-09-22 17:53:14 +0900

    Function for player choice and algorithm for checking answers

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 function getComputerChoice() {
     let choices = ["Rock", "Paper", "Scissor"];
     let randomChoice = Math.floor(Math.random() * choices.length);
-    console.log(choices[randomChoice])
     return choices[randomChoice];
 
 }
-getComputerChoice();
+
+function playerSelection() {
+ let input = prompt("Rock, Paper or Scissors?");
+ input = input.toLowerCase();
+ formattedChoice = input.charAt(0).toUpperCase() + input.slice(1);
+
+ if (formattedChoice == getComputerChoice()) {
+    console.log(`Damn son. You got that bitch with your badass ${formattedChoice}!!`);
+ }
+ else {
+    console.log(`Computer picked: ${getComputerChoice()}...You lose`);
+ }
+
+ return formattedChoice 
+}
+
+playerSelection();
